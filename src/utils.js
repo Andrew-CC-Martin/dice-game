064deae8108f9f3eda3368e8a4bdd47bb52107cc
Andrew-CC-Martin <amartin262@gmail.com>
2021-04-17 11:47:56 +1000

    add fade effect

--- src/utils.js
+++ src/utils.js
@@ -2,5 +2,9 @@ export const dice = () => Math.floor(Math.random() * 6) + 1
 
 export const updateImage = (diceImage) => {
   const number = dice()
-  diceImage.src = `./images/${number}.png`
+  diceImage.classList.add('transparent')
+  setTimeout(() => {
+    diceImage.src = `./images/${number}.png`
+    diceImage.classList.remove('transparent')
+  }, 200);
 }
